362f8a120e6d7bbd894b3bf37657645c98fc2bd4
Rodrigo Nazareth <rodrigonazareth.analista@gmail.com>
2022-03-13 01:53:36 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/dto/MovieDTO.java
+++ backend/src/main/java/com/devsuperior/dsmovie/dto/MovieDTO.java
@@ -1,35 +1,19 @@
-package com.devsuperior.dsmovie.entities;
+package com.devsuperior.dsmovie.dto;
 
-import java.util.HashSet;
-import java.util.Set;
+import com.devsuperior.dsmovie.entities.Movie;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "tb_movie")
-public class Movie {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+public class MovieDTO {
 	private Long id;
 	private String title;
 	private Double score;
 	private Integer count;
 	private String image;
 	
-	@OneToMany(mappedBy = "id.movie")
-	private Set<Score> scores = new HashSet<>();
-	
-	public Movie() {
+	public MovieDTO() {
 		
 	}
 
-	public Movie(Long id, String title, Double score, Integer count, String image) {
-		super();
+	public MovieDTO(Long id, String title, Double score, Integer count, String image) {
 		this.id = id;
 		this.title = title;
 		this.score = score;
@@ -37,57 +21,53 @@ public class Movie {
 		this.image = image;
 	}
 	
+	public MovieDTO(Movie movie) {
+		id = movie.getId();
+		title = movie.getTitle();
+		score = movie.getScore();
+		count = movie.getCount();
+		image = movie.getImage();
+	}
+
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public String getTitle() {
 		return title;
 	}
-
 
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
-
 	public Double getScore() {
 		return score;
 	}
-
 
 	public void setScore(Double score) {
 		this.score = score;
 	}
 
-
 	public Integer getCount() {
 		return count;
 	}
-
 
 	public void setCount(Integer count) {
 		this.count = count;
 	}
 
-
 	public String getImage() {
 		return image;
 	}
 
-
 	public void setImage(String image) {
 		this.image = image;
 	}
-
-	public Set<Score> getScores() {
-		return scores;
-	}		
+	
 	
 }
