e56bef27e8ea8d262faaf0ff0a52dd1dd1c30233
Rodrigo Nazareth <rodrigonazareth.analista@gmail.com>
2022-03-17 22:09:31 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,18 +1,36 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/request";
 
 function Listing() {
 
+    // ()response => { -> Isto é uma passagem de função para outra função executar
     // FORMA ERRADA: APENAS TESTE
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-        .then(response => {
-            console.log(response.data)
-        });
+    // Desse executa mais de uma vez
+    // axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    //     .then(response => {
+    //         console.log(response.data)
+    //     });
+
+    // Hook: useState -> Manter estado no componente
+    const [pageNumber, setPageNumber] = useState(0);
+
+    // Hook: useEffect ->
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                console.log(data);
+                setPageNumber(data.number);
+            });
+    }, []);
 
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination />
             <div className="container">
                 <div className="row">
